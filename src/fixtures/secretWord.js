17f5debc2a44b0d1a014ad6b28dfec5be3e098fc
dov11 <ovo.dan@gmail.com>
2017-12-03 00:43:28 +0100

    add some random words

--- src/fixtures/secretWord.js
+++ src/fixtures/secretWord.js
@@ -1,4 +1,20 @@
-const secretWord = "hello"
+const words = [
+              'hello',
+              'world',
+              'word',
+              'rails',
+              'jazz',
+              'late',
+              'night',
+              'days',
+              'javascript',
+              'random',
+              'maiden',
+              'someone',
+              'music'
+            ]
+
+const secretWord = words[Math.floor(Math.random()*words.length)]
                 .split("")
                 .map(letter=> ({letter, guessed: false}))
 
