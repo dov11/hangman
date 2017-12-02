df2c90f2920777cb4afc85107ec63996f22b897c
dov11 <ovo.dan@gmail.com>
2017-12-02 22:54:53 +0100

    add lives left and hangman picture

---
+++ src/reducers/progress.js
@@ -0,0 +1,17 @@
+import { GUESS_LETTER } from '../actions/guess'
+import secretWord from '../fixtures/secretWord'
+const livesLeftStart = 6
+
+export default (state = livesLeftStart, { type, payload } = {}) => {
+  switch(type) {
+    case GUESS_LETTER :
+    // const correctClassName = secretWord.map(a=>a.letter).includes(payload.letter) ? "correct" : "incorrect"
+    if (!secretWord.map(a=>a.letter).includes(payload.letter)){
+      return state -1
+    } else {
+    return state
+  }
+    default :
+      return state
+  }
+}
